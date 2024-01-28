3ab621835e2bb01f7f9da89b0d83ef578b294afb
Victor Romanov <gerter666@hotmail.com>
2024-01-28 16:09:58 +0300

    #1 combat logic adapted, all parts implemented

--- UserCampaigns/WP/M1/Combat/CursedRock/script.lua
+++ UserCampaigns/WP/M1/Combat/CursedRock/script.lua
@@ -14,15 +14,26 @@ cursed_rock_fight =
     -- существо, на которое был скастован фантом на текущей итерации
     current_creature_to_phantom = "",
     -- таблица информации о базовых отрядах для перемещения фантомов
-    replace_info = {}
+    replace_info = {},
+    -- флаг активности полёта рохи(игра считает, что юнит в полете не существует, поэтому засчитывает поражение)
+    rock_dive_flag = nil,
+    -- ход, на который роха использует полёт
+    rock_dive_turn = -1000,
+    -- текущий ход рохи
+    rock_current_turn = -1
 }
 
 if GetGameVar("cursed_rock_fight") == "1" then
-    AddCombatFunction(CombatFunctions.START,
+    if initialDifficulty == DIFFICULTY_NORMAL or initialDifficulty == DIFFICULTY_HARD then
+        cursed_rock_fight.rock_dive_turn = 3
+    elseif initialDifficulty == DIFFICULTY_HEROIC then
+        cursed_rock_fight.rock_dive_turn = 2
+    end
+    AddCombatFunction(CombatFunctions.START, "gog_m1_cursed_rock_fight_start",
     function()
         startThread(CursedRockFight_Start)
     end)
-    AddCombatFunction(CombatFunctions.DEFENDER_CREATURE_MOVE,
+    AddCombatFunction(CombatFunctions.DEFENDER_CREATURE_MOVE, "gog_m1_cursed_rock_fight_defender_move",
     function(creature)
         CursedRock_RockMove(creature, DEFENDER)
         if cursed_rock_fight.stage == CURSED_ROCK_FIGHT_STAGE_PREPARATION then
@@ -69,11 +80,13 @@ function CursedRockFight_WinThread()
             Finish(DEFENDER)
         end
         if not next(GetDefenderCreatures()) then
-            if check then
-                AddCreature(ATTACKER, CREATURE_GREMLIN, 1)
-                check = nil
+            if not cursed_rock_fight.rock_dive_flag then
+                if check then
+                    AddCreature(ATTACKER, CREATURE_GREMLIN, 1)
+                    check = nil
+                end
+                Finish(ATTACKER)
             end
-            Finish(ATTACKER)
         end
         sleep()
     end
@@ -84,10 +97,17 @@ end
 -- 2) Убрать базовые отряды
 -- 3) Вызвать существо, которое будет фантомить
 function CursedRockFight_Start()
+    EnableAutoFinish(nil)
     -- 1.
     for i, creature in GetAttackerCreatures() do
         local cx, cy = pos(creature)
-        cursed_rock_fight.replace_info[creature] = {type = GetCreatureType(creature),  count = GetCreatureNumber(creature), x = cx, y = cy}
+        cursed_rock_fight.replace_info[creature] = {
+            type = GetCreatureType(creature),  
+            count = GetCreatureNumber(creature), 
+            x = cx, 
+            y = cy, 
+            --index = i + len(GetAttackerCreatures())
+        }
     end
     -- 2.
     for creature, count in attacker_real_army do
@@ -96,12 +116,12 @@ function CursedRockFight_Start()
     -- 3.
     local helper = "cursed_rock_fight_helper"
     local rock_x, rock_y = pos(GetDefenderCreatures()[0])
-    if pcall(AddCreature, ATTACKER, HELPER, 10, rock_x, rock_y, nil, helper) then
+    if pcall(AddCreature, ATTACKER, HELPER, 10, 8, 8, nil, helper) then
         while not exist(helper) do
             sleep()
         end
+        displace(helper, 13, 60)
     end
-    --
 end
 
 -- ход рохи.
@@ -130,11 +150,15 @@ function CursedRock_RockMove(creature, side)
         end
         -- 1c
         if not curr_info then
-            EnableAutoFinish(nil)
+            print("All phantomed")
+            cursed_rock_fight.rock_current_turn = 0
             startThread(CursedRockFight_WinThread)
             cursed_rock_fight.stage = CURSED_ROCK_FIGHT_STAGE_ACTIVE
             EnableCombat()
             removeUnit("cursed_rock_fight_helper")
+            while exist("cursed_rock_fight_helper") do
+                sleep()
+            end
             return 1
         end
         -- 1d
@@ -150,5 +174,55 @@ function CursedRock_RockMove(creature, side)
         commandDefend(creature)
         EndTurn(creature, 1)
         return 1
+    else
+        cursed_rock_fight.rock_current_turn = cursed_rock_fight.rock_current_turn + 1
+        if mod(cursed_rock_fight.rock_current_turn, cursed_rock_fight.rock_dive_turn) == 0 then
+            combatSetPause(1)
+            --EnableCinematicCamera(nil)
+            SetUnitManaPoints(creature, 1)
+            while GetUnitManaPoints(creature) ~= 1 do
+                sleep()
+            end
+            -- pcall(UnitCastAimedSpell, creature, 355, GetAttackerCreatures()[0])
+            -- sleep(30)
+            cursed_rock_fight.rock_dive_flag = 1
+            local x, y = pos(GetAttackerCreatures()[0])
+            if pcall(commandDoSpecial, creature, SPELL_ABILITY_PACK_DIVE, x, y) then
+                combatSetPause(nil)
+                return 1
+            end
+        else
+            if cursed_rock_fight.rock_dive_flag == 1 then
+                while not exist(creature) do
+                    sleep()
+                end
+                cursed_rock_fight.rock_dive_flag = nil
+            end
+            --
+            local targets_near, t_n = {}, 0
+            for i, _cr in GetAttackerCreatures() do
+                local dist = CheckDist(creature, _cr)
+                if dist <= 1 then
+                    t_n = t_n + 1
+                    targets_near[t_n] = _cr
+                end
+            end
+            --
+            for i, spawn in GetSpellSpawns(ATTACKER) do
+                local dist = CheckDist(creature, spawn)
+                if dist <= 1 then
+                    t_n = t_n + 1
+                    targets_near[t_n] = spawn
+                end
+            end
+            --
+            if t_n >= 2 then
+                for i, target in targets_near do
+                    commandMoveAttack(creature, target)
+                    sleep()
+                end
+                return 1
+            end
+        end
     end
 end
