372375e719e7ba28b2e21d19ca55c8023260b750
pegn0ly <owafe001@gmail.com>
2025-08-26 18:31:21 +0300

    #7 cleanup

--- UserCampaigns/Prologue/M2/Quests/Primary/UnknownPath/Dialogs/SecondObeliskDialog/script.lua
+++
@@ -1,6 +0,0 @@
-MiniDialog.Sets["C1M2_DIALOG_2"] = {
-	["0_main"] = {speaker = "Karlam", speaker_type = SPEAKER_TYPE_HERO},
-	["1_main"] = {speaker = CREATURE_MAGI, speaker_type = SPEAKER_TYPE_CREATURE},
-	["2_main"] = {speaker = "Karlam", speaker_type = SPEAKER_TYPE_HERO},
-}
-
