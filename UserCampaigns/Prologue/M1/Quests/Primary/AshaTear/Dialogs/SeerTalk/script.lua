0715d1b3a50c805b0f221d3a5fa1697e792c7ad6
pegn0ly <owafe001@gmail.com>
2026-06-16 01:12:28 +0300

    old type scripts cleanup in c1m1

--- UserCampaigns/Prologue/M1/Quests/Primary/AshaTear/Dialogs/SeerTalk/script.lua
+++
@@ -1,9 +0,0 @@
-MiniDialog.Sets["C1M1_DIALOG_08"] = {
-	["0_main"] = {speaker = "Karlam", speaker_type = SPEAKER_TYPE_HERO},
-	["1_main"] = {speaker = CREATURE_HIGH_DRUID, speaker_type = SPEAKER_TYPE_CREATURE},
-	["2_main"] = {speaker = "Karlam", speaker_type = SPEAKER_TYPE_HERO},
-	["3_main"] = {speaker = CREATURE_HIGH_DRUID, speaker_type = SPEAKER_TYPE_CREATURE},
-	["4_main"] = {speaker = "Karlam", speaker_type = SPEAKER_TYPE_HERO},
-	["5_main"] = {speaker = CREATURE_HIGH_DRUID, speaker_type = SPEAKER_TYPE_CREATURE},
-}
-
